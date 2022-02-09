d221c33cac0458488b0174414add868eca7c9629
Khalia Parris <khaliaparris01@gmail.com>
2022-02-09 14:50:14 -0500

    setInterval opperations

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const box1 = document.getElementById("1")
 
-
+const box2 = document.getElementById("2")
 
 // box1.style.backgroundColor = "black"
 // box1.style.width = "100px"
 // box.style.height = "100px"
 
 
-let boxtest = 2; 
-
-
-
-console.log("#" + boxtest.toString())
-/// this box should be able to make the box blink for one sec and then stop after a second 
-function blinkBox(boxId){
- $("#" + boxId.toString()).fadeOut(1000).fadeIn(1000);   
+let boxTest = 1; 
+let pattern = [1,3]
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {resolve('')},ms)
+  })
+  
 }
 
-/// this is an example using j.query 
-// $("#Test").fadeOut(1000).fadeIn(1000);
+async function startGame(){
+  
+for(let i =0; i <pattern.length; i ++){
+
+  blinkBox(pattern[i])
+
+  await sleep(2000)
+}
+
+}
+
+function blinkBox(boxId){
+    $("#" + boxId.toString()).fadeOut(1000).fadeIn(1000);
+    
+  
+}
+// $("#" + el.toString()).fadeOut(1000).fadeIn(1000);  
+  
+
 
 
 box1.addEventListener("click", ()=>{
-  blinkBox(boxtest)  
+  startGame();  
 })
+
