62a343ac791a0cd83409fdc348873dea6bc57ba8
leo <cedillo47@gmail.com>
2022-02-09 17:17:09 +0000

    made blink func dynamic

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const box = document.getElementById("Test")
-
-box.style.backgroundColor = "black"
-box.style.width = "100px"
-box.style.height = "100px"
+const box1 = document.getElementById("1")
 
 
 
+// box1.style.backgroundColor = "black"
+// box1.style.width = "100px"
+// box.style.height = "100px"
 
+
+let boxtest = 2; 
+
+
+
+console.log("#" + boxtest.toString())
 /// this box should be able to make the box blink for one sec and then stop after a second 
-function blinkBox(){
- $("#Test").fadeOut(1000).fadeIn(1000);   
+function blinkBox(boxId){
+ $("#" + boxId.toString()).fadeOut(1000).fadeIn(1000);   
 }
 
 /// this is an example using j.query 
 // $("#Test").fadeOut(1000).fadeIn(1000);
 
 
-box.addEventListener("click",blinkBox)
+box1.addEventListener("click", ()=>{
+  blinkBox(boxtest)  
+})
