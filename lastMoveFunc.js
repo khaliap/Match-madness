ab206db8ef5ef66e507bc2c82a8be3ae57a29caf
leo <cedillo47@gmail.com>
2022-02-10 19:06:27 +0000

    last move cherker func compleate

---
+++ lastMoveFunc.js
@@ -0,0 +1,125 @@
+const box1 = document.getElementById("1")
+
+const box2 = document.getElementById("2")
+
+// box1.style.backgroundColor = "black"
+// box1.style.width = "100px"
+// box.style.height = "100px"
+
+// box2 = document.getElementById()
+
+const box3 = document.getElementById("3")
+
+const box4 = document.getElementById("4")
+
+const message = document.getElementById('resultDis')
+const startBtn = document.querySelector('button')
+//reslut input 
+let resultArr = [];
+
+//
+let pattern = [1,4]
+
+let currentIndex = 0;
+
+// function ClickOnBox{
+//     resultArr.push()
+// }
+
+
+
+
+box1.addEventListener("click",()=>{
+    resultArr.push(getID(box1))
+    checkIndex()
+    currentIndex ++
+})
+
+box2.addEventListener("click",()=>{
+  resultArr.push(getID(box2))
+  checkIndex()
+  currentIndex ++
+})
+
+
+box3.addEventListener("click",()=>{
+  resultArr.push(getID(box3))
+  checkIndex()
+  currentIndex ++
+  LastMove()
+})
+
+
+box4.addEventListener("click",()=>{
+  resultArr.push(getID(box4))
+  checkIndex()
+  currentIndex ++
+})
+
+
+console.log(resultArr)
+
+function checkIndex(){
+    if(resultArr[currentIndex] !== pattern[currentIndex]){
+        message.innerHTML = "wrong"
+    }
+    return true; 
+  }
+
+  console.log(resultArr)
+function getID(currBox){
+    return Number(currBox.id)
+}
+
+
+
+console.log(getID(box3))
+
+
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {resolve('')},ms)
+  })
+  
+}
+
+async function startGame(){
+  
+for(let i =0; i <pattern.length; i ++){
+
+  blinkBox(pattern[i])
+
+  await sleep(2000)
+}
+
+}
+
+function blinkBox(boxId){
+    $("#" + boxId.toString()).fadeOut(1000).fadeIn(1000);
+    
+}
+// $("#" + el.toString()).fadeOut(1000).fadeIn(1000);  
+
+
+function LastMove(){
+    if(currentIndex === pattern.length){
+         message.innerHTML = "good"
+         console.log("kshbckhsbckhsdb")
+    }
+}  
+
+const board = document.getElementById("game-grid")
+
+
+/// add last move func to all boxes, 
+
+///
+board.addEventListener("click", LastMove())
+
+startBtn.addEventListener("click", ()=>{
+  startGame();  
+})
+console.log("hello")
