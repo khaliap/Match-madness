a8bacf69597fa1bd16655048661381416d357937
Khalia Parris <khaliaparris01@gmail.com>
2022-02-14 14:59:12 -0500

    removed files

--- level.js
+++ level.js
@@ -2,43 +2,38 @@ const box1 = document.getElementById("1")
 
 const box2 = document.getElementById("2")
 
-// box1.style.backgroundColor = "black"
-// box1.style.width = "100px"
-// box.style.height = "100px"
-
-// box2 = document.getElementById()
-
 const box3 = document.getElementById("3")
 
 const box4 = document.getElementById("4")
 
 const message = document.getElementById('resultDis')
 const startBtn = document.querySelector('button')
+
 //reslut input 
 let resultArr = [];
 
+let randomOutput =[]
+
+let num = 3
 //
-let pattern = [1,4,3]
+let pattern = randomOutput
 
 let currentIndex = 0;
-
-// function ClickOnBox{
-//     resultArr.push()
-// }
-
-
-
 
 box1.addEventListener("click",()=>{
     resultArr.push(getID(box1))
     checkIndex()
     currentIndex ++
+      LastMove()
+     
 })
 
 box2.addEventListener("click",()=>{
   resultArr.push(getID(box2))
   checkIndex()
   currentIndex ++
+    LastMove()
+   
 })
 
 
@@ -46,6 +41,8 @@ box3.addEventListener("click",()=>{
   resultArr.push(getID(box3))
   checkIndex()
   currentIndex ++
+  LastMove()
+  
 })
 
 
@@ -53,6 +50,8 @@ box4.addEventListener("click",()=>{
   resultArr.push(getID(box4))
   checkIndex()
   currentIndex ++
+    LastMove()
+    
 })
 
 
@@ -60,20 +59,28 @@ console.log(resultArr)
 
 function checkIndex(){
     if(resultArr[currentIndex] !== pattern[currentIndex]){
-        message.innerHTML = "wrong"
+        message.innerHTML = "Wrong"
+        // levelText.innerHTML = '1'
     }
+    return true; 
+ 
   }
 
-  console.log(resultArr)
+  // console.log(resultArr)
 function getID(currBox){
     return Number(currBox.id)
 }
 
-let levelText = document.getElementById('level')
-console.log(levelText)
-console.log(getID(box3))
 
-
+// console.log(getID(box3))
+// console.log(levelText)
+const levelText = document.querySelector('span')
+function levelTextFunc() {
+  
+  let number = levelText.innerHTML;
+  number++;
+  levelText.innerHTML = number;
+}
 
 
 
@@ -81,31 +88,62 @@ function sleep(ms) {
   return new Promise(resolve => {
     setTimeout(() => {resolve('')},ms)
   })
-  
 }
+
 
 async function startGame(){
-  
 for(let i =0; i <pattern.length; i ++){
-
   blinkBox(pattern[i])
-
-  await sleep(2000)
+  await sleep(1000)
 }
-
 }
 
 function blinkBox(boxId){
-    $("#" + boxId.toString()).fadeOut(1000).fadeIn(1000);
+    $("#" + boxId.toString()).fadeOut(200).fadeIn(200);
     
-  
 }
 // $("#" + el.toString()).fadeOut(1000).fadeIn(1000);  
+
+
+function LastMove(){
+    if(currentIndex === pattern.length){
+         message.innerHTML = "Level Complete"
+      
+         num++;
+         currentIndex = 0;
+         resultArr = [];
+         randomOutput = [];
+         getRandomInt()
+         pattern = randomOutput;
+         levelTextFunc()
+    }
+}  
+
+// const board = document.getElementById("game-grid")
+
+
+/// add last move func to all boxes, 
+
+///
+// board.addEventListener("click", LastMove())
   
-
-
-
 startBtn.addEventListener("click", ()=>{
-  startGame();  
+  getRandomInt();
+  // startGame();  
 })
+
+
+  let randomOptions = [1,2,3,4]
+
+function getRandomInt() {
+  
+while(randomOutput.length < num ){
+  let randomPattern = randomOptions[Math.floor(Math.random()* randomOptions.length)];
+  randomOutput.push(randomPattern)
+}
+startGame();  
+}
+
+
+
 
